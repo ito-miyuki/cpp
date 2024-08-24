ecc09edb3fd3840125d08a4b742d59a46e517363
Miyuki Ito <miyuki.ito@slurp.coffee>
2024-08-24 09:05:28 +0300

    deleted unnecessary spaces

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -4,18 +4,12 @@
 int main()
 {
     std::string     str = "HI THIS IS BRAIN";
-    std::string*    stringPTR = &str; // A pointer to str.
-    std::string&    stringREF = str; // A reference to the string
+    std::string*    stringPTR = &str;
+    std::string&    stringREF = str;
 
     std::cout << "Address of str: " << &str << std::endl;
     std::cout << "Memory address held by stringPTR: " << stringPTR << std::endl;
     std::cout << "Memory address held by stringREF: " << &stringREF << std::endl;
-
-    /*
-    The value of the string variable.
-    • The value pointed to by stringPTR.
-    • The value pointed to by stringREF
-    */
 
     std::cout << "value of the string: " << str << std::endl;
     std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
