42d82ba409cf3a1e9226a861a7ab3ecc62755c9f
Miyuki Ito <miyuki.ito@slurp.coffee>
2024-11-14 10:02:27 +0200

    added 06

---
+++ cpp06/ScalarConverter.hpp
@@ -0,0 +1,15 @@
+#ifndef SCALARCONVERTER
+# define SCALARCONVERTER
+
+class ScalarConverter {
+
+    public:
+        ScalarConverter();
+        ~ScalarConverter();
+        ScalarConverter(const ScalarConverter& other);
+
+        ScalarConverter& operator=(const ScalarConverter& other);
+
+};
+
+#endif 
